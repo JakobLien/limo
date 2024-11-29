ff60cb0b40aff815a7d180d7374cc9e8bf78bb67
JakobLien <jakoblien01@gmail.com>
2024-11-29 19:25:05 +0800

    Screen controllable driving in visualize.py

--- visualize.py
+++ visualize.py
@@ -43,6 +43,12 @@ def getAngle(i):
 def getAverage(*points):
     return [sum([p[0] for p in points])/len(points), sum([p[1] for p in points])/len(points)]
 
+# Fleir program her:
+# Det utkommenterte e at den kjøre mot den lengst unna lidar målinga den finn
+# Så e koden som ligg her no at man kan styr hjulan sin hastighet (opp/ned) og svinging(høyre/venstre) ved å rør på skjermen
+# også vises også koordinaten av fingern øverst te venstre, og vi tegne en stor blå sirkel under fingern. 
+
+relCord = (0, 0)
 while True:
     # Kryss ut eller CTRL + C for å stopp programmet
     if any([e.type == pygame.QUIT for e in pygame.event.get()]) or rospy.is_shutdown():
@@ -68,21 +74,40 @@ while True:
         # Lysintensiteten e et godt mål på om det e en god måling, virke som om alle ekte målinga har lysintensitet over 1000 (av 1024 anntar e)
         # Fortsatt en par feilmålinger med dette, men det kan vi fjern effektivt via fleir målinger på rad. 
 
+        # TODO: Det står i ROS dokumentasjonen at meldingen kjem med en lavre og øvre lysintensitet, så bruk det istedet!
+
         pygame.draw.circle(screen, "black", (300 + int(100 * point[0]), 300 - int(100 * point[1])), 3)
 
-    if points:
-        furthest_point = max([p for p in points if p[1] > abs(p[0])], key=lambda p: p[0]**2 + p[1]**2)
-        pygame.draw.circle(screen, "red", (300 + int(100 * furthest_point[0]), 300 - int(100 * furthest_point[1])), 5)
+    pygame.draw.circle(screen, "pink", relCord, 3)
 
-        cmd_vel.linear.x = 0.05 * math.sqrt(furthest_point[0]**2 + furthest_point[1]**2)
-        cmd_vel.angular.z = -furthest_point[0] / 20
-        cmd_vel_pub.publish(cmd_vel)
-    else:
-        print('points empty')
+    f = pygame.font.Font(size=32)
+    
+    screen.blit(f.render(f'({round(relCord[0], 2)}, {round(relCord[1], 2)})', True, "black", "white"), (30, 10))
+
+    if pygame.mouse.get_pressed()[0]:
+        pygame.draw.circle(screen, "blue", pygame.mouse.get_pos(), 30)
+        relCord = ((pygame.mouse.get_pos()[0] - 300) / 300, (pygame.mouse.get_pos()[1] - 300) / 300)
+        # print(relCord)
+
+    # if points:
+    #     furthest_point = max([p for p in points if p[1] > abs(p[0])], key=lambda p: p[0]**2 + p[1]**2)
+    #     pygame.draw.circle(screen, "red", (300 + int(100 * furthest_point[0]), 300 - int(100 * furthest_point[1])), 5)
+
+    #     cmd_vel.linear.x = 0.05 * math.sqrt(furthest_point[0]**2 + furthest_point[1]**2)
+    #     cmd_vel.angular.z = -furthest_point[0] / 20
+    #     cmd_vel_pub.publish(cmd_vel)
+    # else:
+    #     print('points empty')
+
+    cmd_vel.linear.x = -relCord[1]
+    cmd_vel.angular.z = -relCord[0] * -relCord[1] # Dette konvertere fra turning velocity (grader) te faktisk wheel position, så e smoothar for dette:)
+    cmd_vel_pub.publish(cmd_vel)
 
     pygame.display.flip() # Draw the screen
 
     clock.tick(10)  # 10 FPS
+
+    relCord = (relCord[0] / 1.1, relCord[1] / 1.1)
 
 # Stopp ROS om vi kryssa ut
 rospy.signal_shutdown(0)
